694dc8c88777732b331598158798dbfdd4f94e95
spandemic <anloeng@ucsc.edu>
2022-11-01 16:23:27 -0700

    added spinning cones inside sphere

--- source/main.js
+++ source/main.js
@@ -1,4 +1,4 @@
-let r;
+let r, r2;
 let theta;
 let phi;
 let x, y, z;
@@ -8,6 +8,7 @@ function setup() {
     angleMode(DEGREES);
 
     r = 240;
+    r2 = 210;
   }
   
 function draw() {
@@ -19,14 +20,15 @@ function draw() {
     // y = r(sin(phi)sin(theta))
     // z = r(cos(phi))
     
-    for (let phi = -180; phi <= 180; phi += 15) {
-        for (let theta = 0; theta < 360; theta += 30) {
+    // outer swarm
+    for (let theta = 30; theta < 330; theta += 15) {
+        for (let phi = 0; phi < 360; phi += 30) {
             x = r * sin(phi) * cos(theta);
             y = r * sin(phi) * sin(theta);
-            z = r * cos(phi)
+            z = r * cos(phi);
             
             push();
-            if (phi % 30 == 0) {
+            if (theta % 30 == 0) {
                 rotateY(frameCount);
                 rotateZ(-frameCount);
             } else {
@@ -38,8 +40,29 @@ function draw() {
             sphere(2);
             pop();
         }
-
     }
-    
+
+    // inner core
+    for (let phi = 15; phi < 345; phi += 15) {
+        push();
+        fill(phi % 180);
+        for (let theta = 0; theta < 360; theta += 15) {
+            x = r2 * sin(phi) * cos(theta);
+            y = r2 * sin(phi) * sin(theta);
+            z = r2 * cos(phi);
+            
+            push();
+            if (phi % 30 == 0) {
+                rotateZ(2 * frameCount);
+            } else {
+                rotateZ(-2 * frameCount);
+            }
+            translate(x, y, z);
+            noStroke();
+            cone(20, 20, 3);
+            pop();
+        }
+        pop();
+    }
 }
   
